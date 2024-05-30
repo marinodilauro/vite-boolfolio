d22e932db407a9d76059094fb11588654ad7613b
marinodilauro <m.dilauro90@gmail.com>
2024-05-30 15:50:43 +0200

    Add api call to print results in console

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      base_api_url: 'http://127.0.0.1:8000',
+      base_project_url: '/api/projects',
+      projects: [],
+    }
+  },
+  methods: {
+    callApi(url) {
+      axios
+        .get(url)
+        .then(response => {
+          console.log(response);
+          this.projects = response.data.projects;
+          this.loading = false;
+        })
+        .catch(errr => {
+          console.error(err);
+        })
+    }
+  },
+  mounted() {
+    let url = this.base_api_url + this.base_project_url;
+    this.callApi(url);
+  }
+}
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
